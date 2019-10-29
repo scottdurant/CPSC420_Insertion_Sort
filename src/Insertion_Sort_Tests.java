8f5efba67a2d03cda458073e62ac9132ce54da58
Scott Durant <scott.durant.16@cnu.edu>
2019-10-29 11:11:16 -0400

    added junit test file

---
+++ src/Insertion_Sort_Tests.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Insertion_Sort_Tests {
+
+    @Test
+    void test() {
+        fail("Not yet implemented");
+    }
+
+}
