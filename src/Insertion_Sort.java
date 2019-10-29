ff04e5e6bd2250a9ddd43024b28b26e6fbeb194c
Scott Durant <scott.durant.16@cnu.edu>
2019-10-29 10:53:28 -0400

    created class insertion_sort

---
+++ src/Insertion_Sort.java
@@ -0,0 +1,12 @@
+
+public class Insertion_Sort {
+
+    public Insertion_Sort() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world.");
+    }
+
+}
