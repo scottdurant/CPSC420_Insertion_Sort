e9e72fc85fd71e603029935d71c9ddba714ff050
Scott Durant <scott.durant.16@cnu.edu>
2019-11-18 15:50:06 -0500

    fininshing touches

--- src/Insertion_Sort.java
+++ src/Insertion_Sort.java
@@ -11,27 +11,27 @@ public class Insertion_Sort {
         
         ArrayList<Character> listToSort = new ArrayList<Character>();
         
+        // put the input into an ArrayList
         for (int i=0; i<userInput.length(); i++) {
             listToSort.add(userInput.charAt(i));
         }
         
-        System.out.println("Sorting list: " + listToSort);
-        ArrayList<Character> sortedList = new ArrayList<Character>();
-        sortedList = performInsertionSort(listToSort);
-        System.out.println("Sorted list: " + sortedList);
+        System.out.println("Sorting list: " + listToSort + " with insertion sort...");
+        ArrayList<Character> sortedList = performInsertionSort(listToSort);
+        System.out.println("Done");
     }
     
     
     
     
     /**
-     * Accepts an arraylist and returns it sorted using insertion sort.
-     * Follows the psuedo code from class
+     * Accepts an arraylist and returns the arraylist sorted using insertion sort.
+     * Based on the psuedo code from class
      */
     public static ArrayList<Character> performInsertionSort(ArrayList<Character> list){
         int n = list.size();
-        System.out.println("Size = " + n);
         
+        // index starts at 1 because index 0 is considered sorted
         for (int i = 1; i < n; i++) {
             Character v = list.get(i);
             int j = i - 1;
